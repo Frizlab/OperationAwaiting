fa409bbcdb102efc7f032d7b13476d7b93952b2e
Frizlab <fload@me.com>
2021-12-07 15:49:50 +0100

    Remove requirement for nil completion block when adding operations to a queue

--- Sources/OperationAwaiting/OperationQueue+Async.swift
+++ Sources/OperationAwaiting/OperationQueue+Async.swift
@@ -14,13 +14,12 @@ public extension OperationQueue {
 	}
 	
 	func addOperationAndWait(_ op: Operation) async {
-		assert(op.completionBlock == nil, "Async support for Operation, as implemented, requires the completion block to be left nil.")
 		await withTaskCancellationHandler(handler: { op.cancel() }, operation: {
 			await withCheckedContinuation{ (continuation: CheckedContinuation<Void, Never>) -> Void in
-				op.completionBlock = {
-					continuation.resume()
-				}
-				addOperation(op)
+				let completionOperation = BlockOperation{ continuation.resume() }
+				completionOperation.addDependency(op)
+				
+				addOperations([op, completionOperation], waitUntilFinished: false)
 			}
 		})
 	}
@@ -31,13 +30,12 @@ public extension OperationQueue {
 	}
 	
 	func addOperationAndGetResult<O : Operation>(_ op: O) async throws -> O.ResultType where O : HasResult {
-		assert(op.completionBlock == nil, "Async support for Operation, as implemented, requires the completion block to be left nil.")
 		return try await withTaskCancellationHandler(handler: { op.cancel() }, operation: {
 			try await withCheckedThrowingContinuation{ (continuation: CheckedContinuation<O.ResultType, Error>) -> Void in
-				op.completionBlock = {
-					continuation.resume(with: op.result)
-				}
-				addOperation(op)
+				let completionOperation = BlockOperation{ continuation.resume(with: op.result) }
+				completionOperation.addDependency(op)
+				
+				addOperations([op, completionOperation], waitUntilFinished: false)
 			}
 		})
 	}
